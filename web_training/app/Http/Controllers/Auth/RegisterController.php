19e4bd49f728f35913e01dd60b7b9883789d3a58
jrpikong <jr.pikong@gmail.com>
2018-10-11 09:58:03 +0700

    fix register controller

--- web_training/app/Http/Controllers/Auth/RegisterController.php
+++ web_training/app/Http/Controllers/Auth/RegisterController.php
@@ -69,18 +69,6 @@ class RegisterController extends Controller
         ]);
     }
 
-    public function register(Request $request)
-    {
-        $this->validator($request->all())->validate();
-
-        event(new Registered($user = $this->create($request->all())));
-
-        // $this->guard()->login($user);
-
-        return $this->registered($request, $user)
-            ?: redirect($this->redirectPath());
-    }
-
     /**
      * Create a new user instance after a valid registration.
      *
