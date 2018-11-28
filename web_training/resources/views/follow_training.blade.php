5b19ecb70804e87d17890ab504ff9a14049151ea
jrpikong <jr.pikong@gmail.com>
2018-11-28 16:50:34 +0700

    fix pdf viewers

--- web_training/resources/views/follow_training.blade.php
+++ web_training/resources/views/follow_training.blade.php
@@ -6,7 +6,11 @@
             <h5 class="card-title">SSA Training </h5>
         </div>
         <div class="card-body">
-            <iframe src ="{{ asset($training->file_module) }}" width="100%" height="600px"></iframe>
+            <div  style="width: 100%; height: 600px; position: relative; ">
+                <iframe  id="iframe" src="http://docs.google.com/gview?url=https://scholar.harvard.edu/files/torman_personal/files/samplepptx.pptx&embedded=true" style="width:100%; height:600px;" frameborder="0">
+                </iframe>
+                <div style="width: 80px; height: 80px; position: absolute; opacity: 0; right: 0px; top: 0px;">&nbsp;</div>
+            </div>
             <div class="text-center pt-3">
                 <h1>Jika Merasa Sudah Cukup Memahami dan Menguasai, Silahkan Klik "TEST" Dibawah Ini Untuk Mengetahui Tingkat Penguasaaan Materi</h1>
                 <a href="{{ route('follow_test',$training->id) }}" class="btn btn-dark btn-block">TEST</a>
@@ -19,4 +23,11 @@
 @push('script')
     <!-- Theme JS files -->
     <script src="{{asset('limitless/assets/js/app.js')}}"></script>
+    <script>
+        var head = $("#iframe").contents().find("head");
+        var css = '<style type="text/css">' +
+            '.ndfHFb-c4YZDc-Wrql6b{display:none}; ' +
+            '</style>';
+        $(head).append(css);
+    </script>
 @endpush
