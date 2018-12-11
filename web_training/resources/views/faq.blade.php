25473f1fd535fed3c65f0a4b9da8e18f7e965335
jrpikong <jr.pikong@gmail.com>
2018-12-11 15:50:12 +0700

    add faq pages

---
+++ web_training/resources/views/faq.blade.php
@@ -0,0 +1,109 @@
+@extends('layouts.front')
+
+@section('content')
+    <section class="page-header page-header-modern bg-color-primary page-header-md">
+        <div class="container">
+            <div class="row">
+
+            </div>
+        </div>
+    </section>
+    <div class="container py-4">
+
+    <div class="row">
+        <div class="col">
+
+            <h2 class="font-weight-normal text-7 mb-2">Frequently Asked <strong class="font-weight-extra-bold">Questions</strong></h2>
+            {{--<p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pellentesque neque eget diam posuere porta. Quisque ut nulla at nunc <a href="#">vehicula</a> lacinia. Proin adipiscing porta tellus, ut feugiat nibh adipiscing sit amet. In eu justo a felis faucibus ornare vel id metus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In eu libero ligula.</p>--}}
+
+            <hr class="solid my-5">
+
+            <div class="toggle toggle-primary" data-plugin-toggle>
+                <section class="toggle active">
+                    <label>Apa itu Samsung Student Ambassador (SSA) ?</label>
+                    <p>Program Samsung yang mewadahi mahasiswa untuk mempromosikan dan menjual produk elektronik seperti TV, kulkas dan mesim cuci melalui microsite Samsung Student Ambassador yang sudah kerja sama dengan Blibli (transaksi melalui link Samsung Student Ambassador.</p>
+                </section>
+
+                <section class="toggle">
+                    <label>Bagaimana cara join Samsung Student Ambassador ?</label>
+                    <p>
+                        <ol>
+                            <li>Status mahasiswa aktif atau baru lulus kuliah</li>
+                            <li>Registrasi melalui http://samsungstudentambassador.com dan mentandatangani perjanjian freelance sebagai Samsung Student Ambassador (sebelum ditandatangani mohon untuk dibaca dan memahami perjanjian freelance tersebut)</li>
+                        </ol>
+                    </p>
+                </section>
+
+                <section class="toggle">
+                    <label>Apa keuntungan/benefit join Samsung Student Ambassador?</label>
+                    <p>
+                        <ol>
+                            <li>menjadi bagian dari perusahaan samsung (10 perusahaan terbaik dunia)</li>
+                            <li>mendapatkan pengalaman sebelum terjun ke dunia kerja</li>
+                            <li>meningkatkan semangat entrepeneur</li>
+                            <li>meningkatkan kepercayaan diri</li>
+                            <li>berkesempatan bergabung dengan perusahaan samsung setelah lulus kuliah</li>
+                            <li>waktu yang fleksibel</li>
+                            <li>mendapatkan pendapatan tambahan sampai Rp 5 juta atau lebih setiap bulannya</li>
+                        </ol>
+                    </p>
+                </section>
+
+                <section class="toggle">
+                    <label>Apa job desk SSA ?</label>
+                    <p>
+                        <ol>
+                            <li>mengikuti training product knowladge dan selling skill yang sudah di sediakan web Samsung Student Ambassador</li>
+                            <li>mengetahui info promo produk yang sedang berjalan</li>
+                            <li>aktif menawarkan produk elektronik untuk menarik pelanggan untuk transaksi</li>
+                            <li>melakukan transaksi hanya melalui microsite samsung student ambassador (link khusus)</li>
+                            <li>melakukan report transaksi kepada admin</li>
+                        </ol>
+                    </p>
+                </section>
+
+                <section class="toggle">
+                    <label>Bagaimana cara transaksi melalui Samsung Student Ambassador ?</label>
+                    <p>
+                        <ol>
+                            <li>klik link https://www.blibli.com/promosi/pel-student-promotor-samsung</li>
+                            <li>pilih produk yang mau di beli dan pilih beli sekarang</li>
+                            <li>masukan akun Blibli jika belum login dan jika belum ada akun blibli registrasi terlebih dahulu</li>
+                            <li>masukan alamat lengkap pembeli untuk pengiriman produk beserta nomor telpon yang aktif</li>
+                            <li>klik lanjutkan pembayaran</li>
+                            <li>pilih metode pembayaran (kartu kredit, kartu debit, cicilan tanpa kartu kredit, internet banking, kredivo, transfer, uang elektronik, pospay) kemudian klik bayar sekarang</li>
+                            <li>transaksi berhasil dan akan muncul kode pesanan serta akan mendapatkan email pemesanan produk</li>
+                        </ol>
+                    </p>
+                </section>
+
+                <section class="toggle">
+                    <label>Berapa besar insentif yang diperoleh ?</label>
+                    <p>
+                        <span>Skema insentif baru</span>
+                        <ul>
+                            <li>1 unit - Rp.4,9jt = Rp.300rb</li>
+                            <li>Rp.5jt - Rp.7,49jt = Rp.400rb</li>
+                            <li>Rp.7,5 - Rp.9,9jt = Rp.600rb</li>
+                            <li>Rp.10jt ke atas = 10% X sell out</li>
+                        </ul>
+                    </p>
+                </section>
+
+                <section class="toggle">
+                    <label>Bagaimana cara klaim insentif yang diterima ?</label>
+                    <p>Report penjualan produk ke admin dengan format</p>
+                    <p>Tanggal transaksi :</p>
+                    <p>Kode pesanan :</p>
+                    <p>Nama produk :</p>
+                    <p>Harga produk :</p>
+                </section>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+@endsection
