341523a8746814192d0bd501bbac9d941d9421b2
jrpikong <jr.pikong@gmail.com>
2018-12-12 01:46:51 +0700

    fix img

--- web_training/app/Http/Controllers/HomeController.php
+++ web_training/app/Http/Controllers/HomeController.php
@@ -84,10 +84,6 @@ class HomeController extends Controller
     public function ssaAgreement()
     {
         $user = Auth::user();
-
-        $user->addMediaFromRequest('img_digital_signature')->toMediaCollection('img_digital_signature');
         return view('ssa_agreement',compact('user'));
     }
-
-
 }
